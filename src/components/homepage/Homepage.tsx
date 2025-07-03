135f47b73b8f00e18d4fac740539a7ce343b9342
RostislavNaumenko <rostislavnaumenko.work@gmail.com>
2025-07-03 20:41:25 +0200

    structure

---
+++ src/components/homepage/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './homePage.module.css'
+
+const Homepage: React.FC = () => {
+    return (
+        <div className={styles.header}>
+            Home
+        </div>
+    );
+}
+
+export default Homepage;
